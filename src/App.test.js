0d9c4a65e32393638ed5f19ddff9f428016c81bd
Gokhan Cihan <cihangokhangmail.com>
2021-08-17 18:04:55 +0300

    test for filtering by text input; userEvent test module, module import

--- src/App.test.js
+++ src/App.test.js
@@ -3,18 +3,23 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import Header from "./Header"
 import { render, screen } from "@testing-library/react"
-
-test("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-});
+import userEvent from '@testing-library/user-event'
 
 test('renders Header component ', () => {
   render(<Header />)
 })
 
-test('emojis are rendered on initial page-load', () => { 
+beforeEach(() => {
   render(<App />)
+})
+
+test('emojis are rendered on initial page-load', () => { 
   const emojiRows = screen.getByTestId("test-on-load")
   expect(emojiRows.querySelectorAll("div>div").length === 20)
+})
+
+test('filters emoji by input', () => {
+  const inputNode = document.getElementsByTagName("input")[0]
+  userEvent.type(inputNode, "kissing heart")
+  expect(screen.getByAltText("Kissing Heart"))
 })
